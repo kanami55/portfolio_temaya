74411ab413259a0b9b448e6c510b04f6d4c62b4a
kanami55 <summerwave5959@gmail.com>
2022-03-04 14:11:00 +0000

    [Add] user一覧画面追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,27 @@
 class UsersController < ApplicationController
 
+ def new
+    @user = User.new
+  end
 
+  def create
+    @user = User.new(user_params)
+    @user.save
+    redirect_to user_path
+  end
+
+  def index
+    @post = Post.includes(:user)
+  end
+
+  def show
+  end
+
+  #def edit
+  #end
+
+  private
+  def user_params
+    params.reqier(:user).permit(:profile_image, :introduction, :name)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   root to: 'homes#top'
 
   resources :posts
-  #resources :users, only: [:new, :create, :index, :show, :edit, :update]
+  resources :users, only: [:new, :create, :index, :show, :edit, :update]
   get "/homes/about" => "homes#about", as: "about"
 end

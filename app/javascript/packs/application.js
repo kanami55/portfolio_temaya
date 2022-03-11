bacb1e5630f3466c0f5fd0c2a3fa5002a9a7d6ff
kanami55 <summerwave5959@gmail.com>
2022-03-11 13:34:16 +0000

    [Fix] 文字のスタイル変更

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -27,14 +27,3 @@ require('packs/raty')
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
-
-const CLASSNAME = "-visible";
-const TIMEOUT = 1500;
-const $target = $(".title");
-
-setInterval(() => {
-  $target.addClass(CLASSNAME);
-  setTimeout(() => {
-    $target.removeClass(CLASSNAME);
-  }, TIMEOUT);
-}, TIMEOUT * 2);

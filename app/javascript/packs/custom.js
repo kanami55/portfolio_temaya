0d104eb109ed1d2e9713459740834b88654ea651
kanami55 <summerwave5959@gmail.com>
2022-03-21 09:38:18 +0000

    [Fix] application.html.erb 部分テンプレートに修正

--- app/javascript/packs/custom.js
+++ app/javascript/packs/custom.js
@@ -1,4 +1,4 @@
-  /*topへ戻る*/
+  //topへ戻る
     jQuery(window).on("scroll", function($) {
       //10pxスクロールしたらボタンが登場
   	if (jQuery(this).scrollTop() > 10) {
@@ -7,10 +7,25 @@
   		jQuery('#page_top').hide();
   	}
   });
-    //ボタンをクリックしたら0.5秒かけて上に戻る
+
+  //ボタンをクリックしたら0.5秒かけて上に戻る
   jQuery('#page_top').click(function () {
   	jQuery('body,html').animate({
   		scrollTop: 0
   	}, 800);
   	return false;
+  });
+
+  //ボタンを押したら指定位置までスクロール
+  $(function() {
+      var home_about = $("#home_about");
+      var body = $("body");
+
+      home_about.click(function() {
+        var  = $(this).attr("href");
+        var scroll_point = $().offset().top;
+        body.animate({
+          scrollTop: scroll_point
+        }, 300);
+      });
   });

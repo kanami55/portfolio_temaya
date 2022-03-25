420a96dba84b5c2d21d473bc9b5cdf67c8f20b1f
kanami55 <summerwave5959@gmail.com>
2022-03-25 11:13:33 +0000

    [Add] post 結合テスト一部追加

--- spec/system/posts_spec.rb
+++ spec/system/posts_spec.rb
@@ -1,104 +1,64 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :system do
-  @user = FactoryBot.create(:user)
-  let(:post) { create(:post) }
-  before do
-    sign_in(:user)
-  end
 
-  describe '新規投稿' do
-    context 'フォームの入力値が正常' do
-      it '新規投稿成功' do
-        # 投稿一覧画面へ遷移
-        visit new_post_path
-        fill_in 'post[title]', with: post.title
-        fill_in 'post[production_period]', with: post.production_period
-        fill_in 'post[cost]', with: post.cost
-        fill_in 'post[explanation]', with: post.explanation
-        fill_in 'post[difficulty]', with: post.difficulty
-        click_button '投稿'
-        # posts_pathへ遷移する事を期待する
-        expect(current_path).to eq posts_path
+
+  describe '投稿のテスト' do
+
+      let(:user) { FactoryBot.create(:user, name: 'test')}
+      let!(:post) { FactoryBot.create(:post, user: user) }
+
+
+    before do
+      visit new_user_session_path
+      fill_in 'user[name]', with: user.name
+      fill_in 'user[password]', with: user.password
+      click_button 'ログイン'
+    end
+
+    describe 'navberへの表示テスト' do
+      context '表示の確認' do
+        it '投稿すると表示される' do
+          expect(page).to have_content '投稿する>'
+          expect(current_path).to eq posts_path
+        end
       end
     end
 
-    context '画像投稿未入力' do
-      it '新規投稿失敗' do
-        visit new_post_path
-        fill_in 'post[title]', with: post.title
-        fill_in 'post[production_period]', with: post.production_period
-        fill_in 'post[cost]', with: post.cost
-        fill_in 'post[explanation]', with: post.explanation
-        fill_in 'post[difficulty]', with: post.difficulty
-        click_button '投稿'
-        expect(current_path).to eq new_post_path
-        expect(page).to have_content "Photoを入力してください"
+    describe '投稿のテスト' do
+      context '新規投稿ページへ遷移' do
+        it '遷移する' do
+          click_on '投稿'
+          expect(page).to have_content '新規投稿'
+          expect(current_path).to eq new_post_path
+        end
       end
-    end
 
-    context 'タイトル未入力' do
-      it '新規投稿失敗' do
-        fill_in 'post[title]', with: nil
-        fill_in 'post[production_period]', with: post.production_period
-        fill_in 'post[cost]', with: post.cost
-        fill_in 'post[explanation]', with: post.explanation
-        fill_in 'post[difficulty]', with: post.difficulty
-        click_button '投稿'
-        expect(current_path).to eq new_post_path
-        expect(current_path).to have_content "Titleを入力してください"
-      end
-    end
-
-    context '製作期間未入力' do
-      it '新規投稿失敗' do
-        fill_in 'post[title]', with: post.title
-        fill_in 'post[production_period]', with: nil
-        fill_in 'post[cost]', with: post.cost
-        fill_in 'post[explanation]', with: post.explanation
-        fill_in 'post[difficulty]', with: post.difficulty
-        click_button '投稿'
-        expect(current_path).to eq new_post_path
-        expect(page).to have_content "ProductionPeriodを入力してください"
-      end
-    end
-
-    context '製作費用未入力' do
-      it '新規投稿失敗' do
-        fill_in 'post[title]', with: post.title
-        fill_in 'post[production_period]', with: post.production_period
-        fill_in 'post[cost]', with: nil
-        fill_in 'post[explanation]', with: post.explanation
-        fill_in 'post[difficulty]', with: post.difficulty
-        click_button '投稿'
-        expect(current_path).to eq new_post_path
-        expect(page).to have_content "Costを入力してください"
-      end
-    end
-
-    context '難易度未入力' do
-      it '新規投稿失敗' do
-        fill_in 'post[title]', with: post.title
-        fill_in 'post[production_period]', with: post.production_period
-        fill_in 'post[cost]', with: post.cost
-        fill_in 'post[explanation]', with: post.explanation
-        fill_in 'post[difficulty]', with: nil
-        click_button '投稿'
-        expect(current_path).to eq new_post_path
-        expect(page).to have_content "Difficultyを入力してください"
-      end
-    end
-
-    context '説明未入力' do
-      it '新規投稿失敗' do
-        fill_in 'post[title]', with: post.title
-        fill_in 'post[production_period]', wit: post.production_period
-        fill_in 'post[cost]', with: post.cost
-        fill_in 'post[explanation]', with: nil
-        fill_in 'post[difficulty]', with: post.difficulty
-        click_button '投稿'
-        expect(current_path).to eq new_post_path
-        expect(page).to have_content "Explanationを入力してください"
+      context '表示の確認' do
+        before do
+          visit new_post_path
+        end
+          it '画像投稿フォームが表示される' do
+            expect(page).to have_filed 'article[image]'
+          end
+          it 'タイトルフォームが表示される' do
+            expect(page).to have_filed 'タイトル：'
+          end
+          it '製作期間フォームが表示される' do
+            expect(page).to have_filed '製作期間：'
+          end
+          it '製作費用フォームが表示される' do
+            expect(page).to have_filed '製作費用：'
+          end
+          it '難易度フォームが表示される' do
+            expect(page).to have_filed '難易度：'
+          end
+          it '説明フォームが表示される' do
+            expect(page).to have_filed '説明：'
+          end
+          it '投稿ボタンが表示される' do
+            expect(page).to have_button '投稿する'
+          end
       end
     end
   end

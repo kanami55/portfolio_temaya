beaf936c8f6f411223c260483d4b1d6715da82da
kanami55 <summerwave5959@gmail.com>
2022-04-05 12:41:03 +0000

    [Fix] users_controller posts_conntroller修正

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -29,6 +29,11 @@ class PostsController < ApplicationController
 
   def edit
     @post = Post.find(params[:id])
+    if @post.user == current_user
+      render "edit"
+    else
+      redirect_to posts_path
+    end
   end
 
   def update
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,8 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!, except: [:index]
   before_action :ensure_guest_user, only: [:edit]
+  before_action :ensure_correct_user, only: [:edit, :update]
+
   def new
     @user = User.new
   end
@@ -79,6 +81,13 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     if @user.name == "guestuser"
       redirect_to user_path(current_user), notice: 'ゲストユーザーはプロフィール編集画面へ遷移できません。'
+    end
+  end
+
+  def ensure_correct_user
+    @user = User.find(params[:id])
+    if @user != current_user
+      redirect_to user_path(current_user)
     end
   end
 end
